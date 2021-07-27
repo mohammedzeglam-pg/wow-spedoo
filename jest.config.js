67f7d8ce3af65426d0325590de0b44e424de1f0b
mohammedzeglam-pg <mohammedzeglam@gmail.com>
2021-07-27 15:50:34 +0200

    angular fresh start

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,6 @@ module.exports = {
     '<rootDir>/libs/config',
     '<rootDir>/libs/ui',
     '<rootDir>/libs/auth-ui',
-    '<rootDir>/libs/ui-user',
     '<rootDir>/libs/file-handler',
     '<rootDir>/libs/tasks',
   ],
